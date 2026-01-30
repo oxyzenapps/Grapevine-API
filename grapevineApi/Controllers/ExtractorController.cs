1d59fd06eb048ca195d6bfcc7c56c56eaf27c2be
Udai Goyal <oxyzen.app@gmail.com>
2026-01-31 00:38:43 +0530

    Extraction update

--- grapevineApi/Controllers/ExtractorController.cs
+++ grapevineApi/Controllers/ExtractorController.cs
@@ -16,9 +16,9 @@ namespace grapevineApi.Controllers
         {
             _env = env;
         }
-        [Route("lands/in-mh/bhulekh")]
+        [Route("lands/in-mh/bhulekh-7-12")]
         [HttpGet]
-        public async Task<IActionResult> BhulekhExtractor(string District, string Taluka, string Village, string SurveyNumber1, string SurveryNumber, string MobileNo, string Language)
+        public async Task<IActionResult> BhulekhExtractor(string District, string Taluka, string Village, string SurveyNumber, string HissaNumber, string MobileNo, string Language)
         {
             //var exePath = @"D:\Office Projects\Grapevine\Grapevine_website\BhulekhExtractor\BhulekhExtractor.exe";
             var exePath = Path.Combine(
@@ -36,11 +36,64 @@ namespace grapevineApi.Controllers
                 District = District,
                 Taluka = Taluka,
                 Village = Village,
-                SurveyNumber1 = SurveyNumber1,
-                SurveryNumber = SurveryNumber,
+                SurveyNumber = SurveyNumber,
+                HissaNumber = HissaNumber,
                 MobileNo = MobileNo,
                 Language = Language,
-                Url = url
+                Url = url,
+                UrlType = "Bhulekh"
+            };
+            var json = JsonConvert.SerializeObject(payload);
+            string escapedJson = json.Replace("\"", "\\\"");
+            var psi = new ProcessStartInfo
+            {
+                FileName = exePath,
+                Arguments = $"\"{escapedJson}\"",   // ✅ quoted
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            using (var process = Process.Start(psi))
+            {
+                var outputTask = process.StandardOutput.ReadToEndAsync();
+                var errorTask = process.StandardError.ReadToEndAsync();
+                process.WaitForExit();
+
+                var output = await outputTask;
+                var error = await errorTask;
+
+                if (!string.IsNullOrWhiteSpace(error))
+                    JsonConvert.DeserializeObject(error);
+
+                return Ok(output);
+            }
+        }
+
+        [Route("lands/in-mh/circlerates")]
+        [HttpGet]
+        public async Task<IActionResult> CircleRatesExtractor(string District, string Taluka, string Village, string SurveyNumber, string HissaNumber, string MobileNo, string Language)
+        {
+            //var exePath = @"D:\Office Projects\Grapevine\Grapevine_website\BhulekhExtractor\BhulekhExtractor.exe";
+            var exePath = Path.Combine(
+    _env.ContentRootPath,
+    "BhulekhExtractor",
+    "publish",
+    "BhulekhExtractor.exe"
+);
+            string url = "https://igreval.maharashtra.gov.in/eASR2.0/eASRCommon.aspx";
+            
+            var payload = new ScrapeRequest
+            {
+                District = District,
+                Taluka = Taluka,
+                Village = Village,
+                SurveyNumber = SurveyNumber,
+                HissaNumber = HissaNumber,
+                MobileNo = MobileNo,
+                Language = Language,
+                Url = url,
+                UrlType = "CircleRate"
             };
             var json = JsonConvert.SerializeObject(payload);
             string escapedJson = json.Replace("\"", "\\\"");
@@ -71,23 +124,30 @@ namespace grapevineApi.Controllers
 
         [Route("lands/in-mh/bhunaksha")]
         [HttpGet]
-        public async Task<IActionResult> BhuNakshaExtractor(string District, string Taluka, string Village)
+        public async Task<IActionResult> BhuNakshaExtractor(string District, string Taluka, string Village,string SurveyNumber,string HissaNumber,string MobileNo,string Language)
         {
             //var exePath = @"D:\Office Projects\Grapevine\Grapevine_website\BhulekhExtractor\BhulekhExtractor.exe";
             var exePath = Path.Combine(
     _env.ContentRootPath,
-    "BhuNaksha",
+    "BhulekhExtractor",
     "publish",
-    "BhuNaksha.exe"
+    "BhulekhExtractor.exe"
 );
-            string url = "https://igreval.maharashtra.gov.in/eASR2.0/eASRCommon.aspx";
-            
+            string url = "https://mahabhunakasha.mahabhumi.gov.in/27/index.html";
+            District = await TransliterateToMarathi(District);
+            Taluka = await TransliterateToMarathi(Taluka);
+            Village = await TransliterateToMarathi(Village);
             var payload = new ScrapeRequest
             {
                 District = District,
                 Taluka = Taluka,
                 Village = Village,
-                Url = url
+                SurveyNumber = SurveyNumber,
+                HissaNumber = HissaNumber,
+                MobileNo = MobileNo,
+                Language = Language,
+                Url = url,
+                UrlType = "BhuNaksha"
             };
             var json = JsonConvert.SerializeObject(payload);
             string escapedJson = json.Replace("\"", "\\\"");
@@ -122,10 +182,11 @@ namespace grapevineApi.Controllers
             public string District { get; set; } = "";
             public string Taluka { get; set; } = "";
             public string Village { get; set; } = "";
-            public string SurveyNumber1 { get; set; } = "";
-            public string SurveryNumber { get; set; } = "";
+            public string SurveyNumber { get; set; } = "";
+            public string HissaNumber { get; set; } = "";
             public string MobileNo { get; set; } = "";
             public string Language { get; set; } = "";
+            public string UrlType { get; set; } = "";
         }
 
         [NonAction]
